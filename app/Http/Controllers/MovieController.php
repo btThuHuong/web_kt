40be56597cc1167ab5ad2c86fb5a63d3e1d47e7e
btThuHuong <030239230066@st.buh.edu.vn>
2026-04-08 13:47:26 +0700

    trang chủ + trang thể loại

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
-    public function index(){
-        return view("movie.index");
+    public function index($id = null) {
+        // Hiển thị 12 bộ phim theo yêu cầu
+        $query = DB::table('movie');
+
+        if ($id) {
+            //trang thể loại
+            $query->join('movie_genre', 'movie.id', '=', 'movie_genre.id_movie')
+                ->where('movie_genre.id_genre', $id)
+                ->select('movie.*')
+                ->distinct();
+
+            $genreName = DB::table('genre')->where('id', $id)->value('genre_name_vn');
+            $title = "Thể loại: " . $genreName;
+        }
+        else {
+            // trang chủ
+            $query->where('popularity', '>', 450)
+                ->where('vote_average', '>', 7);
+            $title = "Trang chủ Movie";
+        }
+
+        // Chung: Sắp xếp giảm dần, lấy 12 phim
+        $movies = $query->orderBy('release_date', 'desc')
+                        ->limit(12)
+                        ->get();
+
+        $genre = DB::table('genre')->get();
+
+        return view('movie.index', compact('movies', 'genre', 'title'));
     }
 }
--- resources/views/movie/index.blade.php
+++ resources/views/movie/index.blade.php
@@ -1,5 +1,15 @@
 <x-movie-layout>
-    <x-slot name="title">
-        Movie
-    </x-slot>
+    <x-slot name="title">Trang Chủ Movie</x-slot>
+
+    <div class="list-movie">
+        @foreach($movies as $item)
+            <div class="movie">
+                <img src="{{ asset('storage/' . $item->image) }}" alt="{{ $item->movie_name_vn }}">
+                <div class="movie-info">
+                    <h3>{{ $item->movie_name_vn }}</h3>
+                    <p>{{ $item->release_date }}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </x-movie-layout>

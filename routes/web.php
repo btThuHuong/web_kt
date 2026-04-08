631ff3317744b557df58be3be51890e4f97e2d7e
lovdihov <leloc161205@gmail.com>
2026-04-08 15:16:08 +0700

    Trang quản lý phim

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
 
 
 Route::get('/openrouter', [OpenRouterController::class, 'chat']);
+
+use App\Http\Controllers\MovieController3; 
+Route::get('/movie', [MovieController3::class, 'index'])->name('movie.list');
+Route::get('/movie/delete/{id}', [MovieController3::class, 'destroy'])->name('movie.delete');
